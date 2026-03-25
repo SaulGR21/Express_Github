2b0a7907c04d035d9d32cca237abdad40a3fbc9c
Saul Guzman <saulkikerman@gmail.com>
2026-03-24 21:04:15 -0700

    Mi primera REST API

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+
+app.get('/', (req, res) => {  // Ruta principal
+  res.send('API funcionando');
 });
 
+
+app.get('/usuario', (req, res) => {  // Endpoint GET /usuario
+  const usuario = {
+    id: 1,
+    nombre: 'Saul',
+    rol: 'Estudiante'
+  };
+
+  res.json(usuario);
+});
+
+// Servidor
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
