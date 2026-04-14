954ca1620dd42a9a5f6d0c73ba961a18f492fcca
Saul Guzman <saulkikerman@gmail.com>
2026-04-13 22:55:17 -0700

    Primer SELECT de alumnos con PostreSQL y Express

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
+//Ruta con SELECT para la tabla alumno
 app.get('/alumnos', async (req, res) => {
   try {
-    const resultado = await pool.query('SELECT * FROM alumno');
-    res.json(resultado.rows);
+    const resultado = await pool.query('SELECT * FROM alumno'); // Consultamos a la base de datos react_express_db
+    res.json(resultado.rows); // Enviamos los datos al cliente (navegador o Postman)
   } catch (error) {
     console.error('Error al consultar alumnos:', error);
     res.status(500).json({ error: 'Error al obtener los alumnos' });
